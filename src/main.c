dae17aa73794fb52de1166db2a81bb0e7b17d10e
You thought it was Pawel <50871830+kociad@users.noreply.github.com>
2020-01-21 20:05:32 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,15 +7,15 @@ int main(void)
     DDRB = 1 << PB1;
     PORTB = PORTB | (1 << PB7); //PIN7 je vstup s rezistorem
 
-    while (1)
+    while (1) //Nekonečná smyčka
         {
-        if (!(PINB & (1 << PB7)))
-            for(int E=0;E<=5;E++)
+        if (!(PINB & (1 << PB7))) //Reaguje na stisk tlačítka
+            for(int E=0;E<=5;E++) //Cyklus počítá do 5ti
                 {
                     for(int count=0;count <=500;count++)
                         {   
                             PORTB ^= (1 << PB1);
-                            _delay_ms(2);
+                            _delay_ms(2); //Časová prodleva mezi jednotlivými impulzy=frekvence
                         }
                     _delay_ms(500);
                 }
